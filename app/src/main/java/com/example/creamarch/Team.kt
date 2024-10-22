a7c662b8e333892a1d7292c28c7b0e251c80fc33
Léo Pelazza <leo.pelazza@laposte.net>
2024-10-22 15:22:50 -0400

    Update Team.kt

--- app/src/main/java/com/example/creamarch/Team.kt
+++ app/src/main/java/com/example/creamarch/Team.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -16,19 +18,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.creamarch.ui.theme.CreamarchTheme
 
 
 @Composable
 fun TeamMember(
 	creature: Creature,
+	pv: Int,
 	modifier: Modifier
 )
 {
 	Card(
-		modifier = modifier
-			.padding(10.dp),
+		modifier = modifier,
 		onClick = {}
 	)
 	{
@@ -46,7 +50,7 @@ fun TeamMember(
 			)
 
 			Column(
-				modifier = modifier
+				modifier = modifier.weight(1f)
 			) {
 				Text(
 					text = creature.baseData.name,
@@ -59,6 +63,11 @@ fun TeamMember(
 
 				)
 			}
+			Text(
+				text = "$pv / $pv",
+				modifier = modifier
+					.wrapContentHeight()
+			)
 		}
 	}
 }
@@ -71,14 +80,22 @@ fun TeamMenu(modifier: Modifier = Modifier)
 	// TODO ensure there are no more than 6 creatures in the team
 	var playerTeam = (1..6).map { Dex.species.values.random().spawnNewCreature() }
 
-	LazyColumn(
-		modifier = modifier
-	) {
+
+	LazyColumn (modifier = modifier) {
 		items(playerTeam) {
 			TeamMember(
 				creature = it,
-				modifier = modifier
-			)
+				pv = 100,
+				modifier = Modifier.padding(10.dp))
 		}
+	}
+}
+
+@Composable
+@Preview(showBackground = true)
+fun TeamPreview()
+{
+	CreamarchTheme {
+		TeamMenu()
 	}
 }
